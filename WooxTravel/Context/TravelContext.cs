8f69d349ef6eb9aa86bb4b3e60edc6edef252a29
Selin Karslı <selin.krsli@gmail.com>
2024-10-08 21:56:34 +0300

    Ders-9

---
+++ WooxTravel/Context/TravelContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WooxTravel.Entities;
+
+namespace WooxTravel.Context
+{
+    public class TravelContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ WooxTravel/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WooxTravel.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
