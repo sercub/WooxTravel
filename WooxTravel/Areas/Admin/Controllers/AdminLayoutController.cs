47dc3daceba35ccd5e923b77ac840999e4cd2b96
Selin Karslı <selin.krsli@gmail.com>
2024-10-10 22:22:06 +0300

    Ders-10

--- WooxTravel/Areas/Admin/Controllers/AdminLayoutController.cs
+++ WooxTravel/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -12,5 +12,25 @@ namespace WooxTravel.Areas.Admin.Controllers
         {
             return View();
         }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
