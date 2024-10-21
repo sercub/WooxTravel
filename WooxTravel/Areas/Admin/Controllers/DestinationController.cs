0839e002aca19113e0801a7ff63d4b0767d0cc6e
Selin Karslı <selin.krsli@gmail.com>
2024-10-21 21:28:48 +0300

    Ders-11(Bölüm1)

---
+++ WooxTravel/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WooxTravel.Context;
+using WooxTravel.Entities;
+
+namespace WooxTravel.Areas.Admin.Controllers
+{
+    public class DestinationController : Controller
+    {
+        TravelContext context = new TravelContext();
+        public ActionResult DestinationList()
+        {
+            var values = context.Destinations.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateDestination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateDestination(Destination destination)
+        {
+            context.Destinations.Add(destination);
+            context.SaveChanges();
+            return RedirectToAction("DestinationList", "Destination", "Admin");//Area olduğu için area ismini de eklemem gerekiyor.
+        }
+        public ActionResult DeleteDestination(int id)
+        {
+            var value = context.Destinations.Find(id);
+            context.Destinations.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("DestinationList", "Destination", "Admin");
+        }
+    }
+}
