b669bf986ce8355737577bb19f6c62d508fc92cd
Selin Karslı <selin.krsli@gmail.com>
2024-11-17 02:05:39 +0300

    Added CreditCard Script and Chart&Statistic Controller

--- WooxTravel/Areas/Admin/Controllers/ChartController.cs
+++ WooxTravel/Areas/Admin/Controllers/ChartController.cs
@@ -12,16 +12,48 @@ namespace WooxTravel.Areas.Admin.Controllers
         private TravelContext context = new TravelContext();
         public ActionResult Index()
         {
-            var destinations = context.Destinations.Select(s => new
-            {
-                s.Title,
-                s.Country,
-                s.Price,
-                s.Capacity,
-            }).Take(4).ToList();
-
-            ViewBag.DestinationDatas = destinations;
             return View();
         }
+        public JsonResult ChartData()
+        {
+            var capacityCounts = context.Destinations
+                .GroupBy(s => s.City)
+                .Select(l => new { City = l.Key, Capacity = l.Sum(n=>n.Capacity) })
+                .ToList();
+
+            return Json(capacityCounts, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult RadarChartData()
+        {
+            var radarData = context.Destinations
+                .Select(d => new { d.City, d.Capacity }) 
+                .ToList();
+            var labels = radarData.Select(d => d.City).ToArray(); 
+            var data = radarData.Select(d => d.Capacity).ToArray(); 
+
+            return Json(new { labels, data }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult LineChartData()
+        {
+            var lineData = context.Destinations
+                .Select(d => new { d.City, d.Capacity }) // Şehir ve kapasite değerlerini direkt alıyoruz
+                .ToList();
+
+            var labels = lineData.Select(d => d.City).ToArray(); // Şehir isimleri
+            var data = lineData.Select(d => d.Capacity).ToArray(); // Kapasiteler
+
+            return Json(new { labels, data }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult PieChartData()
+        {
+            var priceCounts = context.Destinations
+            .GroupBy(s => s.City)
+            .Select(l => new { City = l.Key, Price = l.Sum(n => n.Price) })
+            .ToList();
+
+            return Json(priceCounts, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ WooxTravel/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WooxTravel.Context;
+
+namespace WooxTravel.Areas.Admin.Controllers
+{
+    public class StatisticController : Controller
+    {
+        private TravelContext context = new TravelContext();
+
+        public ActionResult Index()
+        {
+            var userInfo = Session["x"];
+
+            var email = context.Admins.Where(s=>s.Username==userInfo).Select(l=>l.Email).FirstOrDefault();
+
+            var totalDestinations = context.Destinations.Count();
+            var longestTourDuration = context.Destinations.Max(d => (int?)d.DayNight) ?? 0; 
+            var mostExpensiveTour = context.Destinations.OrderByDescending(d => d.Price).FirstOrDefault()?.Title;
+            var sendBoxMessageCount = context.Messages.Where(x => x.SenderMail == email) .ToList().Count();
+            var receivedMessagesCount = context.Messages.Where(x => x.ReceiverMail == email).ToList().Count();
+
+            var maxTourCapacity = context.Destinations.Max(d => d.Capacity);
+            var totalCategories = context.Categories.Count();
+
+            var thankYouMessagesCount = context.Messages.Count(m => m.Subject.Contains("Teşekkür"));
+            var totalAdmins = context.Admins.Count(); 
+            var internationalToursCount = context.Destinations.Count(d => d.Country != "Türkiye"); 
+            var totalMessages = context.Messages.Count();
+            var complaintMessagesCount = context.Messages.Count(m => m.Subject.Contains("Şikayet"));
+
+            ViewBag.TotalDestinations = totalDestinations;
+            ViewBag.MostExpensiveTour = mostExpensiveTour;
+            ViewBag.SentMessagesCount = sendBoxMessageCount;
+            ViewBag.ReceivedMessagesCount = receivedMessagesCount;
+
+
+            ViewBag.MaxTourCapacity = maxTourCapacity;
+            ViewBag.TotalCategories = totalCategories;
+
+            ViewBag.ThankYouMessagesCount = thankYouMessagesCount;
+            ViewBag.TotalAdmins = totalAdmins;
+            ViewBag.InternationalToursCount = internationalToursCount;
+            ViewBag.TotalMessages = totalMessages;
+            ViewBag.ComplaintMessagesCount = complaintMessagesCount;
+            ViewBag.DayNightCount = longestTourDuration;
+
+            return View();
+        }
+    }
+}
